1b0b38a6c3d4ca2ca18ffe7c293336581cf4fee5
brnooliveira <brno.oliveiras@gmail.com>
2024-01-09 23:20:57 -0300

    Implementacao dos primeiros endpoints

---
+++ UdemyCourseRestApi/Controllers/VillaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyCourseRestApi.Data;
+using UdemyCourseRestApi.Models.Dto;
+
+namespace UdemyCourseRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+
+        [HttpGet("id")]
+        public VillaDto GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
+    }
+}
---
+++ UdemyCourseRestApi/Models/Dto/VillaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace UdemyCourseRestApi.Models.Dto
+{
+    public class VillaDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
